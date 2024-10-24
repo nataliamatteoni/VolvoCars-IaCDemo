f498287ead2a793f3f50c170d74bee7cefacfc78
Natalia <nataliamatteoni@gmail.com>
2024-10-24 12:04:11 -0300

    first commit

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI ID, mude conforme necessário
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "WebServer"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y docker
+              service docker start
+              docker run -d -p 80:80 nome_da_imagem_no_dockerhub
+              EOF
+}
+
+output "public_ip" {
+  value = aws_instance.web.public_ip
+}

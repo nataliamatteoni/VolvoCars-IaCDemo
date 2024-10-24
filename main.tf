2119196db10da7116a5c42326b9e015555399768
Natalia <nataliamatteoni@gmail.com>
2024-10-24 12:39:00 -0300

    adding security groups

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami           = "ami-06b21ccaeff8cd686" # Amazon Linux 2 AMI ID, mude conforme necessário
   instance_type = "t2.micro"
+  security_groups = [aws_security_group.web_sg.name]  # Associar o grupo de segurança
 
   tags = {
     Name = "WebServer"
@@ -21,4 +22,23 @@ resource "aws_instance" "web" {
 
 output "public_ip" {
   value = aws_instance.web.public_ip
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "web_sg"
+  description = "Allow HTTP traffic"
+  
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Permite acesso de qualquer IP
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # Permite todo o tráfego de saída
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
